dd33eb727195d7e8a5618c13ba69285884fbb989
Max Torres <maxtorres487@gmail.com>
2021-07-08 20:48:22 -0400

    Add value store files

---
+++ lib/values/my_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color colorPrimary = Color(0xFF6D9D8F);
+  static const Color colorLight = Color(0xFF95C0B9);
+  static const Color colorDark = Color(0xFF437F74);
+  static const Color colorAccent = Color(0xFFE4E4C6);
+}
