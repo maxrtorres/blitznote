bbf53840b345f75aa0d6b8236502038a94557b12
Max Torres <maxtorres487@gmail.com>
2021-07-24 19:08:06 -0400

    Search notes by query string (case-insensitive)

--- lib/screens/search_tab.dart
+++ lib/screens/search_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:notes_app/values/keys.dart';
 import 'package:notes_app/values/my_colors.dart';
+import 'package:notes_app/values/strings.dart';
 
 class SearchTab extends StatefulWidget {
   @override
@@ -10,6 +11,7 @@ class SearchTab extends StatefulWidget {
 
 class _SearchTabState extends State<SearchTab> {
   List<dynamic> notes = [];
+  List<dynamic> queryResult = [];
 
   @override
   void initState() {
@@ -27,23 +29,52 @@ class _SearchTabState extends State<SearchTab> {
     }
     setState(() {
       notes = newNotes;
+      queryResult = newNotes;
     });
+  }
+
+  void filter(value) {
+    List<dynamic> newNotes;
+    if (value == null || value == '') {
+      newNotes = notes;
+    } else {
+      newNotes = notes
+          .where((note) =>
+              containsIgnoreCase(note.title, value) ||
+              containsIgnoreCase(note.body, value))
+          .toList();
+    }
+    setState(() {
+      queryResult = newNotes;
+    });
+  }
+
+  bool containsIgnoreCase(String str1, String str2) {
+    return str1.toLowerCase().contains(str2.toLowerCase());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyColors.colorLight,
-      body: new ListView(
-        scrollDirection: Axis.vertical,
-        children: ListTile.divideTiles(
-          context: context,
-          tiles: notes.map((note) => new ListTile(
-                title: new Text(note.title),
-                subtitle: new Text(note.body),
-              )),
-        ).toList(),
-      ),
-    );
+        backgroundColor: MyColors.colorLight,
+        body: Column(children: [
+          TextField(
+            onChanged: (value) => filter(value),
+            decoration: InputDecoration(
+                labelText: Strings.search, suffixIcon: Icon(Icons.search)),
+          ),
+          Expanded(
+            child: new ListView(
+              scrollDirection: Axis.vertical,
+              children: ListTile.divideTiles(
+                context: context,
+                tiles: queryResult.map((note) => new ListTile(
+                      title: new Text(note.title),
+                      subtitle: new Text(note.body),
+                    )),
+              ).toList(),
+            ),
+          )
+        ]));
   }
 }
